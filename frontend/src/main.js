cf1e28b713c7e88dd039e562ef44253af97ccb16
Krzysztof Laskowski <kp.laskowski250@gmail.com>
2020-12-03 22:32:21 +0100

    adding inertia js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
+import { App, plugin } from '@inertiajs/inertia-vue'
 import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.use(plugin)
+
+const el = document.getElementById('app')
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App, {
+    props: {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: name => require(`./Pages/${name}`).default,
+    },
+  }),
+}).$mount(el)
